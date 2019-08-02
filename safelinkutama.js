ac00a92ba66028facc310759575f7b1921c547c8
silentghostaka <46007362+silentghostaka@users.noreply.github.com>
2019-08-03 01:02:30 +0700

    Update safelinkutama.js

--- safelinkutama.js
+++ safelinkutama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="practice-environmental-qwareness-to-our.html"}
 if(x=="2"){xxx="advantages-of-being-home-business-owner.html"}
 if(x=="3"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="4"){xxx="the-way-to-start-business-at-home.html"}a_to_ve[i].href="https://safelink1337.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="the-way-to-start-business-at-home.html"}a_to_ve[i].href="https://safelink1337.blogspot.com/2019/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
